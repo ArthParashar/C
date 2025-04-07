ced45f7d06d4640864a0df09b845a3ab707b7ed1
ArthParashar <111265733+ArthParashar@users.noreply.github.com>
2025-04-07 09:22:30 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,15 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 void main()
 {
-     float l,b,r,pr,pc,ar,ac;
-     printf("Enter the length and width of rectangle and radius of circle \n");
-     scanf("%f %f %f", &l,&b,&r);
-     pr= 2*(l+b);
-     pc= 2*3.14*r;
-     ar= l*b;
-     ac= 3.14*r*r;
-     printf("The perimeter of the rectangle is: %f \n", pr );
-     printf("The area of rectangle is: %f \n" , ar);
-     printf("The perimeter of the circle is: %f \n", pc);
-     printf("The area of circle is: %f \n", ac);
-     }
+   int a,b,c;
+   printf("Enter a value of a and b = \n");
+   scanf("%d %d", &a,&b);
+   c=a;
+   a=b;
+   b=c;
+   printf("Swapped value of a=%d and b=%d", a,b);
+}
