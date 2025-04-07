0ef0228d25a26a64e7515fd094d1b5118dc75020
ArthParashar <111265733+ArthParashar@users.noreply.github.com>
2025-04-07 09:41:21 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main()
-{ 
-   int r;
-   printf("Enter no. of rows");
-   scanf("%d",&r);
-   for(int i=0;i<r;i++)
-   {
-      for(int j=0;j<=i;j++)
-      {
-      printf("*");
-      }
-      printf("\n");
-      }
+{
+  int n,sum=0;
+  printf("Enter a no.\n");
+  scanf("%d",&n);
+  for(int i=1;i<=n;i++)
+  {
+    for(int j=1;j<=i;j++)
+    {
+       sum=sum+j;
+    }
+  }
+  printf("Sum is: %d\n",sum);
 }
