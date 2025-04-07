eb1c5ec944d42fd1307ca41ca9ea0ed32e4b27f9
ArthParashar <111265733+ArthParashar@users.noreply.github.com>
2025-04-07 09:54:07 +0530

    Create print_string.c

---
+++ print_string.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+   char str2[100];
+   char str1[]="Cat";
+   printf("%s\n",str1);
+   printf("Enter a string\n");
+   scanf("%s",str2);
+   printf("%s\n",str2);
+}
